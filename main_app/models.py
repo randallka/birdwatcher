6dcf86282d7213e12451f8c89b25debd424d77c8
randallka-ga <randallkaem@gmail.com>
2023-01-03 17:33:12 -0700

    formatting

--- main_app/models.py
+++ main_app/models.py
@@ -3,32 +3,37 @@ from django.urls import reverse
 from mapbox_location_field.models import LocationField
 
 STATUS = (
-    ('M', 'Migratory'), 
+    ('M', 'Migratory'),
     ('R', 'Residential')
 )
 
-# Create your models here.
-class Bird(models.Model): 
+
+class Bird(models.Model):
     species = models.CharField(max_length=100)
-    migratory = models.CharField("migratory/residential", max_length=1, choices=STATUS, default=STATUS[0][0])
+    migratory = models.CharField(
+        "migratory/residential", max_length=1, choices=STATUS, default=STATUS[0][0])
     description = models.TextField(max_length=200)
 
-    def get_absolute_url(self):   
+    def get_absolute_url(self):
         return reverse('show', kwargs={'bird_id': self.id})
 
+
 class Location(models.Model):
-    location = LocationField(map_attrs={"zoom": 3, "center": [-103.5169725, 38.0354497]})
+    location = LocationField(
+        map_attrs={"zoom": 3, "center": [-103.5169725, 38.0354497]})
     description = models.TextField(max_length=200)
     birds = models.ManyToManyField(Bird)
+
     def get_absolute_url(self):
         return reverse('locations_show', kwargs={'pk': self.id})
 
+
 class Sighting(models.Model):
-    date = models.DateField('Date Seen') 
+    date = models.DateField('Date Seen')
     time = models.TimeField()
     behavior = models.TextField(max_length=150)
     number = models.IntegerField(default='1')
-    bird = models.ForeignKey(Bird, on_delete=models.CASCADE) 
-    class Meta: 
-        ordering = ['date']
+    bird = models.ForeignKey(Bird, on_delete=models.CASCADE)
 
+    class Meta:
+        ordering = ['date']
--- main_app/views.py
+++ main_app/views.py
@@ -4,65 +4,84 @@ from .forms import SightingForm
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
 # Create your views here.
-def home(request): 
+
+
+def home(request):
     return render(request, 'home.html')
 
-def about(request): 
+
+def about(request):
     return render(request, 'about.html')
 
-def birds_index(request): 
-  birds = Bird.objects.all() 
-  return render(request, 'birds/index.html', {'birds' : birds})
 
-def bird_show(request, bird_id): 
-  bird = Bird.objects.get(id=bird_id)
-  sighting_form = SightingForm()
-  return render(request, 'birds/show.html', {'bird' : bird, 'sighting_form' : sighting_form})
+def birds_index(request):
+    birds = Bird.objects.all()
+    return render(request, 'birds/index.html', {'birds': birds})
 
-def add_sighting(request, bird_id): 
-  form = SightingForm(request.POST) 
-  if form.is_valid(): 
-    new_sighting = form.save(commit=False)
-    new_sighting.bird_id = bird_id 
-    new_sighting.save()
-  return redirect('show', bird_id=bird_id) 
 
-class BirdCreate(CreateView): 
-  model = Bird
-  fields = '__all__'
-class BirdUpdate(UpdateView): 
-  model = Bird
-  fields = '__all__'
-class BirdDelete(DeleteView): 
-  model = Bird 
-  success_url = '/birds/'
+def bird_show(request, bird_id):
+    bird = Bird.objects.get(id=bird_id)
+    sighting_form = SightingForm()
+    return render(request, 'birds/show.html', {'bird': bird, 'sighting_form': sighting_form})
 
-class LocationList(ListView): 
-  model = Location
+
+def add_sighting(request, bird_id):
+    form = SightingForm(request.POST)
+    if form.is_valid():
+        new_sighting = form.save(commit=False)
+        new_sighting.bird_id = bird_id
+        new_sighting.save()
+    return redirect('show', bird_id=bird_id)
+
+
+class BirdCreate(CreateView):
+    model = Bird
+    fields = '__all__'
+
+
+class BirdUpdate(UpdateView):
+    model = Bird
+    fields = '__all__'
+
+
+class BirdDelete(DeleteView):
+    model = Bird
+    success_url = '/birds/'
+
+
+class LocationList(ListView):
+    model = Location
+
 
 class LocationShow(DetailView):
-  model = Location
-  def get_context_data(self, **kwargs):
+    model = Location
+
+    def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['bird_list'] = Bird.objects.all()
         return context
 
+
 class LocationCreate(CreateView):
-  model = Location
-  fields = ['location', 'description']
+    model = Location
+    fields = ['location', 'description']
+
 
 class LocationUpdate(UpdateView):
-  model = Location
-  fields = ['location', 'description']
+    model = Location
+    fields = ['location', 'description']
+
 
 class LocationDelete(DeleteView):
-  model = Location
-  success_url = '/locations/'
+    model = Location
+    success_url = '/locations/'
+
 
 def assoc_bird(request, location_id, bird_id):
-  Location.objects.get(id=location_id).birds.add(bird_id)
-  return redirect('locations_show', location_id)
+    Location.objects.get(id=location_id).birds.add(bird_id)
+    return redirect('locations_show', location_id)
 
-def remove_bird(request, location_id, bird_id): 
-  Location.objects.get(id=location_id).birds.remove(bird_id)
-  return redirect('locations_show', location_id)
+
+def remove_bird(request, location_id, bird_id):
+    Location.objects.get(id=location_id).birds.remove(bird_id)
+    return redirect('locations_show', location_id)
